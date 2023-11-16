2c35c69c265ebbcf10854fe1f0eb9242ecd7f02a
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-11-15 23:48:12 -0300

    update temp folder

--- src/config/getTempFolder.ts
+++ src/config/getTempFolder.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 import fs from 'fs';
+import os from 'os';
 import { app } from 'electron';
 
 export function getTempFolder() {
   const localSharerTempFolder = app.isPackaged
     ? path.join(__dirname, 'temp')
-    : path.join(__dirname, '..', '..', 'temp');
+    : path.join(os.homedir(), 'smartShare', 'temp');
 
   if (!fs.existsSync(localSharerTempFolder)) {
     fs.mkdirSync(localSharerTempFolder, { recursive: true });
